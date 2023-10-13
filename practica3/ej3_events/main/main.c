d6ec04d5609f602169dcedc833d5bd0a4ed43fee
marioa25 <marioa25@ucm.es>
2023-10-13 19:58:12 +0200

    Agregada carpeta de ejemplos y el ejemplo user_event_loops

--- practica3/ej3_events/main/main.c
+++ practica3/ej3_events/main/main.c
@@ -8,22 +8,40 @@
 
 #define READ_PERIOD CONFIG_READ_PERIOD
 #define TASK_PRIORITY CONFIG_TASK_PRIORITY
+#define QUEUE_SIZE 5
 
-static const char *TAG = "ej3_events";
-static float temperature = 0;
+static const char *TAG = "ej2_queue`";
 
 void taskFunction(void *parameters);
+
+
+struct taskParam{
+    QueueHandle_t queue;
+    int period;
+}TaskParam;
+
 
 
 void app_main(void)
 {
 
     i2c_master_init();
-    xTaskCreatePinnedToCore(&taskFunction, "TareaMuestreo", 3072, (void *) READ_PERIOD, TASK_PRIORITY, NULL, 0);
-    
+    QueueHandle_t queue = xQueueCreate(QUEUE_SIZE, sizeof(float));
+    struct taskParam params = {
+        .queue=queue, 
+        .period=READ_PERIOD
+    };
+    float readValue = 0.0;
+    BaseType_t statusReturn;
+    const TickType_t ticksToWait = pdMS_TO_TICKS(100);
+
+    xTaskCreatePinnedToCore(&taskFunction, "TareaMuestreo", 3072, (void *) &params, TASK_PRIORITY, NULL, 0);
+    ESP_LOGI(TAG, "***Tarea Principal preparada. Prioridad: %d.***", uxTaskPriorityGet(NULL));
     while(1){
-        vTaskDelay(READ_PERIOD*1000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "Prioridad tarea Main: %d segundos.  Temperatura: %f", uxTaskPriorityGet(NULL), temperature);
+        statusReturn =  xQueueReceive(queue, &readValue, ticksToWait);
+        if(statusReturn == pdTRUE){
+            ESP_LOGI(TAG, "Temperatura: %f", readValue);
+        }
     }
     vTaskDelete(NULL);
 }
@@ -32,11 +50,14 @@ void app_main(void)
 
 void taskFunction(void *parameters){
 
-    int period = (int) parameters;
+    struct taskParam *params = (struct taskParam *) parameters;
+    float temperature = 0.0;
+
+    ESP_LOGI(TAG, "***Tarea Secundaria (muestradora) preparada. Prioridad: %d. Periodo: %d s.***", uxTaskPriorityGet(NULL), params->period);
     while(1){
         readTemperature(I2C_MASTER_NUM, &temperature);
-        ESP_LOGI(TAG, "Prioridad tarea Secundaria: %d.    Periodo de lectura %d segundos.", uxTaskPriorityGet(NULL), period);
-        vTaskDelay(period*1000 / portTICK_PERIOD_MS);
+        xQueueSendToBack(params->queue, &temperature, 0);
+        vTaskDelay(params->period*1000 / portTICK_PERIOD_MS);
     }
     vTaskDelete(NULL);
 }
